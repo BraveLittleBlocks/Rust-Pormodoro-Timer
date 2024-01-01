04859e2cd1d2de1ac7984c1dd0028d847349ee60
Carlos <bravelittleblock@gmail.com>
2024-01-01 16:59:01 -0700

    Added break input and timer

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,10 @@ fn main() {
         let youtube_url_start = "https://www.youtube.com/watch?v=-MaCJZIBKGs";
         let youtube_url_end = "https://youtu.be/2k0SmqbBIpQ?si=2jJzOp_ml2kAwvst&t=2U";
     
-        println!("Choose a timer duration:");
-        println!("1. 1 minute");
-        println!("2. 2 minutes");
+        //First input 
+        println!("Choose a work timer duration:");
+        println!("1. 5 minute");
+        println!("2. 25 minutes");
         println!("3. Exit");
     
         let mut input = String::new();
@@ -17,7 +18,7 @@ fn main() {
     
         let choice = input.trim().parse::<i32>().expect("Invalid input");
     
-        let seconds = match choice {
+        let work_seconds = match choice {
             1 => 300,   // 5 minute
             2 => 1500,  // 25 minutes
             3 => break, // Exit the loop
@@ -26,9 +27,31 @@ fn main() {
                 continue; // Skip to the next iteration of the loop
             }
         };
+        //Second input
+        println!("Choose a Break timer duration:");
+        println!("1. 5 minute");
+        println!("2. 25 minutes");
+        println!("3. Exit");
+    
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed to read line");
+    
+        let choice = input.trim().parse::<i32>().expect("Invalid input");
+    
+        let break_seconds = match choice {
+            1 => 300,   // 5 minute
+            2 => 600,  // 10 minutes
+            3 => break, // Exit the loop
+            _ => {
+                println!("Invalid option");
+                continue; // Skip to the next iteration of the loop
+            }
+        };
+
         webbrowser::open(&youtube_url_start).unwrap();
 
-        let target_time = Instant::now() + Duration::from_secs(seconds);
+        //Work timer
+        let target_time = Instant::now() + Duration::from_secs(work_seconds);
     
         while Instant::now() < target_time {
             // Print remaining time every second
@@ -36,8 +59,17 @@ fn main() {
             println!("Time remaining: {} seconds", remaining.as_secs());
             std::thread::sleep(Duration::from_secs(1));
         }
-    
         webbrowser::open(&youtube_url_end).unwrap(); // Open the URL after the countdown
+
+        //break timer
+        let target_time = Instant::now() + Duration::from_secs(break_seconds);
+    
+        while Instant::now() < target_time {
+            // Print remaining time every second
+            let remaining = target_time - Instant::now();
+            println!("Time remaining: {} seconds", remaining.as_secs());
+            std::thread::sleep(Duration::from_secs(1));
+        }
     }
       
 }
